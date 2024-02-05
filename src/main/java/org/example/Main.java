c7095339204fd6528c4f151ff5f5270522f82bb0
stas <orobtsovv@gmail.com>
2024-02-05 23:09:08 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 2) raiseError(new IllegalArgumentException());
+        try {
+            int[] nums = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+            System.out.println(Arrays.stream(nums).sum());
+        } catch (IllegalArgumentException e) {
+            raiseError(e);
+        }
+    }
+
+    private static void raiseError(Exception e) {
+        System.out.println(e.toString());
+        System.exit(1);
+    }
+}
