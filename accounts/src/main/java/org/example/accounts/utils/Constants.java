56ec35cbbdf2aa3562df29cc13a545ec67356071
stas <orobtsovv@gmail.com>
2024-03-27 19:53:25 +0300

    fix retry for reactive converter

--- accounts/src/main/java/org/example/accounts/utils/Constants.java
+++ accounts/src/main/java/org/example/accounts/utils/Constants.java
@@ -3,7 +3,6 @@ package org.example.accounts.utils;
 public class Constants {
     public static final String INVALID_BIRTHDAY_MESSAGE = "Invalid customer birthday";
     public static final String INVALID_CURRENCY_PARAM_MESSAGE = "Invalid currency";
-    public static final String REGISTRATION_ID = "accounts";
     public static final String NULL_BODY_EXCEPTION = "Null body retrieved from Converter service";
     public static final String CUSTOMER_NOT_FOUND = "Customer not found";
     public static final String CUSTOMER_ACCOUNT_NOT_FOUND = "Customer account not found";
--- converter/src/main/java/org/example/app/client/RatesClient.java
+++ converter/src/main/java/org/example/app/client/RatesClient.java
@@ -3,14 +3,14 @@ package org.example.app.client;
 import io.swagger.client.model.RatesResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.app.exception.NullBodyResponseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Component
@@ -20,18 +20,17 @@ public class RatesClient {
     @Value("${rates-service.rates-path}")
     private final String ratesPath;
 
-    @Retryable(maxAttemptsExpression  = "${rates-service.retry.maxAttempts}",
-            backoff = @Backoff(
-                    delayExpression = "${rates-service.retry.initDelay}",
-                    multiplierExpression = "${rates-service.retry.multiplier}",
-                    maxDelayExpression = "${rates-service.retry.maxDelay}"
-            ))
     public Mono<RatesResponse> getRatesResponse() {
         return webClient.get()
                 .uri(ratesPath)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(RatesResponse.class)
-                .onErrorResume(error -> Mono.error(new NullBodyResponseException()));
+                .retryWhen(Retry
+                        .backoff(3, Duration.ofMillis(50))
+                        .maxBackoff(Duration.ofMillis(150))
+                        .doBeforeRetry(signal -> {
+                            log.info("Retrying...");
+                        }));
     }
 }
