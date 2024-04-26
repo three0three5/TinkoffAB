ba82887a901ae79178e7b44a0b080e14574cdd71
stas <orobtsovv@gmail.com>
2024-04-26 13:58:57 +0300

    cleanup

--- accounts/src/main/java/org/example/accounts/service/FeeService.java
+++ accounts/src/main/java/org/example/accounts/service/FeeService.java
@@ -13,7 +13,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
