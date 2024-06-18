50b1a2595cde2e5274700297ec0514ba6ffd205f
stas <orobtsovv@gmail.com>
2024-06-18 14:55:52 +0300

    add tracing for reactive service

--- converter/src/main/java/org/example/app/Main.java
+++ converter/src/main/java/org/example/app/Main.java
@@ -2,10 +2,12 @@ package org.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(Main.class, args);
     }
 }
